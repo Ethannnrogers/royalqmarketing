1361a08ec1f287bf3835145be6f21e1ed99f785f
Ethan <erogers999@gmail.com>
2019-01-07 22:18:24 -0500

    fixed images, nav shadow, scrolling

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -1,10 +1,16 @@
-$(window).scroll(function() {
-    var y = $(this).scrollTop();
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var y = $(this).scrollTop();
+        
+                if (y > 2) {
+                    $("nav").addClass("nav-shadow");
+                    console.log("scroll");
+                } else {
+                    $("nav").removeClass("nav-shadow");
+                    console.log("unscroll");
+                }
+    })
+        
     
-            if (y > 2) {
-                $("#nav").addClass("nav-shadow");
-            } else {
-                $("nav").removeClass("nav-shadow");
-            }
-})
-    
+});
+
